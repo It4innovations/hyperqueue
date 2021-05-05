a64dbcefcd66300524a74b9a03494824f075cb4f
Stanislav Bohm <spirali@kreatrix.org>
2021-05-05 19:10:23 +0200

    Use std function to generating job name

--- src/client/commands/submit.rs
+++ src/client/commands/submit.rs
@@ -5,18 +5,23 @@ use crate::client::job::print_job_detail;
 use crate::rpc_call;
 use crate::transfer::connection::ClientConnection;
 use crate::transfer::messages::{FromClientMessage, JobStatus, SubmitRequest, ToClientMessage};
+use std::path::PathBuf;
 
 pub async fn submit_computation(
     gsettings: &GlobalSettings,
     connection: &mut ClientConnection,
     commands: Vec<String>,
 ) -> crate::Result<()> {
-    let mut name = &commands
+    let name = commands
         .get(0)
-        .map(|t| extract_task_name_from_path(&t))
+        .and_then(|t| {
+            PathBuf::from(t)
+                .file_name()
+                .and_then(|t| t.to_str().map(|s| s.to_string()))
+        })
         .unwrap_or_else(|| "job".to_string());
     let message = FromClientMessage::Submit(SubmitRequest {
-        name: name.clone(),
+        name,
         cwd: std::env::current_dir().unwrap(),
         spec: ProgramDefinition {
             args: commands,
@@ -31,15 +36,4 @@ pub async fn submit_computation(
     response.job.status = JobStatus::Submitted;
     print_job_detail(gsettings, response.job);
     Ok(())
-}
-
-fn extract_task_name_from_path(job_path: &str) -> String {
-    let mut job_path: String = job_path.to_string();
-    while job_path.ends_with("/") {
-        job_path.pop();
-    }
-    if job_path.is_empty() {
-        job_path = "<empty>".to_string();
-    }
-    job_path.split('/').last().unwrap().to_string()
 }
