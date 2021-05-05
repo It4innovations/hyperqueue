a1e4a62c839d124df4f11937078a062684e53d3e
Vyomkesh <vyomkeshjha@yahoo.com>
2021-05-05 15:10:49 +0200

    extracted task name from path

--- src/client/commands/submit.rs
+++ src/client/commands/submit.rs
@@ -11,12 +11,11 @@ pub async fn submit_computation(
     connection: &mut ClientConnection,
     commands: Vec<String>,
 ) -> crate::Result<()> {
-    // TODO: Strip path
-    let name = commands
+    let mut name = extract_task_name_from_path(&commands
         .get(0)
         .map(|t| t.to_string())
-        .unwrap_or_else(|| "job".to_string());
-
+        .unwrap_or_else(|| "job".to_string())
+    );
     let message = FromClientMessage::Submit(SubmitRequest {
         name: name.clone(),
         cwd: std::env::current_dir().unwrap(),
@@ -33,4 +32,16 @@ pub async fn submit_computation(
     response.job.status = JobStatus::Submitted;
     print_job_detail(gsettings, response.job);
     Ok(())
+}
+
+fn extract_task_name_from_path(job_path: &str) ->String{
+    let mut job_path: String = job_path.to_string();
+    while job_path.ends_with("/") {
+        job_path.pop();
+    }
+    if job_path.is_empty() {
+        job_path = "<empty>".to_string();
+    }
+    let split = job_path.split('/').collect::<Vec<_>>();
+    split[split.len()-1].to_string()
 }
