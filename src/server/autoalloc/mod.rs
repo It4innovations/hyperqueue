d0d3ca1a9a9096e373535eaa0e4baaae81a62597
Jakub Beránek <jakub.beranek@vsb.cz>
2021-08-17 09:51:42 +0200

    Add basic documentation

--- src/server/autoalloc/mod.rs
+++ src/server/autoalloc/mod.rs
@@ -1,3 +1,9 @@
+//! This module controls autoalloc (automatic allocation), a background process that queues jobs
+//! into PBS/Slurm in response to user requirements and task workload to provide more workers for
+//! the HQ runtime.
+//!
+//! The term `allocation` represents a PBS/Slurm job in this module, to distinguish itself from
+//! HQ jobs.
 use thiserror::Error;
 
 pub use process::autoalloc_process;
