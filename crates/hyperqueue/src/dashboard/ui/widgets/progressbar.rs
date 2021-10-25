124f894b883958362c282be54534c0a64e724d26
vyomkeshj <vyomkeshjha@yahoo.com>
2021-10-25 18:10:57 +0200

    Dashboard: Added progressbar

---
+++ crates/hyperqueue/src/dashboard/ui/widgets/progressbar.rs
@@ -0,0 +1,54 @@
+/**
+*   Progress bar's structure: [StartBlock]<>[indicator][][]<>[][][][unused_area]<>[end_block]
+**/
+pub struct ProgressPrintStyle {
+    /// The first character of the progress bar
+    start_block: char,
+    /// The progress indicator
+    indicator: char,
+    /// It fills the empty space between current progress and max progress
+    unused_area: char,
+    /// The ending character of the progressbar
+    end_block: char,
+}
+
+/**
+ * Creates a string progress bar for 0 < progress < 1
+ */
+pub fn render_progress_bar_at(
+    progress: f32,
+    num_characters: u8,
+    style: ProgressPrintStyle,
+) -> String {
+    assert!((0.00..=1.00).contains(&progress));
+    //to keep the length of the progressbar correct after the padding
+    let num_characters = num_characters - 2;
+    let indicator_count = (progress * (num_characters as f32)).ceil();
+    let start_block = style.start_block;
+    let indicator = std::iter::repeat(style.indicator)
+        .take(indicator_count as usize)
+        .collect::<String>();
+    let filler = std::iter::repeat(style.unused_area)
+        .take(num_characters as usize - indicator_count as usize)
+        .collect::<String>();
+
+    format!(
+        "{}{}{}{}: {}%",
+        start_block,
+        indicator,
+        filler,
+        style.end_block,
+        (progress * 100.00) as u32
+    )
+}
+
+impl Default for ProgressPrintStyle {
+    fn default() -> Self {
+        Self {
+            start_block: '╢',
+            end_block: '╟',
+            indicator: '▌',
+            unused_area: '░',
+        }
+    }
+}
