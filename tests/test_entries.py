aa8d826837034cb07872ec7af181ffa3344e3360
Stanislav Bohm <spirali@kreatrix.org>
2021-10-31 20:55:48 +0100

    Add option to create a task array from a JSON file using "hq submit --from-json"

--- tests/test_entries.py
+++ tests/test_entries.py
@@ -1,5 +1,7 @@
 import os
 
+import pytest
+
 from .conftest import HqEnv
 from .utils import wait_for_job_state
 from .utils.job import default_task_output
@@ -61,3 +63,55 @@ def test_entries_with_newline(hq_env: HqEnv):
 
     table = hq_env.command(["job", "1"], as_table=True)
     assert table.get_row_value("State").split("\n")[-1] == "FINISHED (4)"
+
+
+def test_entries_from_json_entry(hq_env: HqEnv):
+    hq_env.start_server()
+    hq_env.start_worker(cpus=2)
+
+    with open("input", "w") as f:
+        f.write('[123, {"x":\n[1,2,3]}, 2.5]')
+
+    hq_env.command(
+        [
+            "submit",
+            "--from-json=input",
+            "--",
+            "bash",
+            "-c",
+            "echo $HQ_ENTRY",
+        ]
+    )
+    wait_for_job_state(hq_env, 1, "FINISHED")
+
+    for i, test in enumerate(["123\n", '{"x":[1,2,3]}\n', "2.5\n"]):
+        with open(default_task_output(job_id=1, task_id=i)) as f:
+            line = f.read()
+        assert line == test
+    assert not os.path.isfile(default_task_output(task_id=3))
+
+    table = hq_env.command(["job", "1"], as_table=True)
+    assert table.get_row_value("State").split("\n")[-1] == "FINISHED (3)"
+
+
+def test_entries_invalid_from_json_entry(hq_env: HqEnv):
+    hq_env.start_server()
+    hq_env.start_worker(cpus=2)
+
+    with open("input", "w") as f:
+        f.write('{"x":\n[1,2,3]}')
+
+    with pytest.raises(
+        Exception,
+        match="The top element of the provided JSON file has to be an array",
+    ):
+        hq_env.command(
+            [
+                "submit",
+                "--from-json=input",
+                "--",
+                "bash",
+                "-c",
+                "echo $HQ_ENTRY",
+            ]
+        )
