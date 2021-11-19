0838152bad16016a094d20b261dc3668381a55b5
Jakub Beránek <berykubik@gmail.com>
2021-11-19 09:30:26 +0100

    Add benchmark that adds a single task to a Core

--- crates/tako/benches/core.rs
+++ crates/tako/benches/core.rs
@@ -3,17 +3,14 @@ use tako::common::Set;
 use tako::server::core::Core;
 use tako::server::task::TaskRef;
 
+fn create_task(id: u64) -> TaskRef {
+    TaskRef::new(id.into(), vec![], Default::default(), 0, false, false)
+}
+
 fn add_tasks(core: &mut Core, count: usize) -> Vec<TaskRef> {
     let mut tasks = Vec::with_capacity(count);
     for id in 0..count {
-        let task = TaskRef::new(
-            (id as u64).into(),
-            vec![],
-            Default::default(),
-            0,
-            false,
-            false,
-        );
+        let task = create_task(id as u64);
         core.add_task(task.clone());
         tasks.push(task);
     }
@@ -65,9 +62,34 @@ fn remove_all_tasks(c: &mut Criterion) {
     }
 }
 
+fn add_task(c: &mut Criterion) {
+    for task_count in [10, 1_000, 100_000] {
+        c.bench_with_input(
+            BenchmarkId::new("add task", task_count),
+            &task_count,
+            |b, &task_count| {
+                b.iter_batched_ref(
+                    || {
+                        let mut core = Core::default();
+                        add_tasks(&mut core, task_count);
+
+                        let task = create_task((task_count + 1) as u64);
+                        (core, task.clone())
+                    },
+                    |(ref mut core, task)| {
+                        core.add_task(task.clone());
+                    },
+                    BatchSize::SmallInput,
+                );
+            },
+        );
+    }
+}
+
 fn core_benchmark(c: &mut Criterion) {
     remove_single_task(c);
     remove_all_tasks(c);
+    add_task(c);
 }
 
 criterion_group!(benches, core_benchmark);
