f4d6c547ccad7f79dbaadc9293dacc08c20326ee
Jakub Beránek <berykubik@gmail.com>
2021-11-23 15:11:26 +0100

    Add HQ server and worker profiling

--- benchmarks/src/environment/hq.py
+++ benchmarks/src/environment/hq.py
@@ -3,8 +3,16 @@ from typing import List
 
 import dataclasses
 
+from ..utils import wait_until
 from ..cluster import ClusterInfo
 from ..cluster.cluster_helper import ClusterHelper, StartProcessArgs
+from ..cluster.profiler import NativeProfiler
+
+
+class ProfileMode:
+    def __init__(self, server=False, workers=False):
+        self.server = server
+        self.workers = workers
 
 
 @dataclasses.dataclass
@@ -12,9 +20,10 @@ class HqClusterInfo:
     cluster: ClusterInfo
     binary: Path
     worker_count: int
+    profile_mode: ProfileMode = dataclasses.field(default_factory=lambda: ProfileMode())
 
     def __post_init__(self):
-        self.binary = self.binary.absolute()
+        self.binary = self.binary.resolve()
 
 
 class HqEnvironment:
@@ -35,33 +44,57 @@ class HqEnvironment:
 
     def start(self):
         self.start_server()
+        self._wait_for_server_start()
+
         self.start_workers(self.worker_nodes[:self.info.worker_count])
         self.cluster.start_monitoring(self.cluster.active_nodes)
         self.cluster.commit()
 
     def start_server(self):
+        workdir = self.server_dir / "server"
+        args = self._profile_args(
+            self._shared_args() + ["server", "start"],
+            self.info.profile_mode.server,
+            workdir / "flamegraph.svg"
+        )
+
         self.cluster.start_processes([StartProcessArgs(
-            args=self._shared_args() + ["server", "start"],
+            args=args,
             host=self.nodes[0],
             name="server",
-            workdir=self.server_dir / "server"
+            workdir=workdir
         )])
 
     def start_workers(self, nodes: List[str]):
-        worker_processes = [
-            StartProcessArgs(
-                args=self._shared_args() + ["worker", "start"],
+        worker_processes = []
+        for (index, node) in enumerate(nodes):
+            workdir = self.server_dir / f"worker-{index}"
+            args = self._profile_args(
+                self._shared_args() + ["worker", "start"],
+                self.info.profile_mode.workers,
+                workdir / "flamegraph.svg"
+            )
+            worker_processes.append(StartProcessArgs(
+                args=args,
                 host=node,
                 name=f"worker-{index}",
-                workdir=self.server_dir / f"worker-{index}"
-            )
-            for (index, node) in enumerate(nodes)
-        ]
+                workdir=workdir
+            ))
 
         self.cluster.start_processes(worker_processes)
 
     def stop(self):
-        self.cluster.stop(True)
+        self.cluster.stop(use_sigint=True)
+
+    def _profile_args(self, args: List[str], profile: bool, path: Path) -> List[str]:
+        if not profile:
+            return args
+        profiler = NativeProfiler()
+        assert profiler.is_available()
+        return profiler.profile(args, path)
 
     def _shared_args(self) -> List[str]:
-        return [self.binary_path, "--server-dir", self.server_dir]
+        return [str(self.binary_path), "--server-dir", str(self.server_dir)]
+
+    def _wait_for_server_start(self):
+        wait_until(lambda: (self.server_dir / "hq-current" / "access.json").is_file())
--- benchmarks/src/utils.py
+++ benchmarks/src/utils.py
@@ -1,5 +1,24 @@
 import os
+import time
 
 
 def get_pyenv_from_env() -> str:
     return os.environ.get("VIRTUAL_ENV")
+
+
+DEFAULT_TIMEOUT = 15
+
+
+class TimeoutException(BaseException):
+    pass
+
+
+def wait_until(fn, sleep_s=0.5, timeout_s=DEFAULT_TIMEOUT):
+    end = time.time() + timeout_s
+
+    while time.time() < end:
+        value = fn()
+        if value is not None and value is not False:
+            return value
+        time.sleep(sleep_s)
+    raise TimeoutException(f"Wait timeouted after {timeout_s} seconds")
