fcbc5d71dafcc7de38b15551008d65ae4500d42e
Jakub Beránek <berykubik@gmail.com>
2022-01-10 18:17:51 +0100

    Add conversion from Rust to Python errors

--- crates/pyhq/src/server.rs
+++ crates/pyhq/src/server.rs
@@ -4,6 +4,7 @@ use hyperqueue::client::default_server_directory_path;
 use hyperqueue::server::bootstrap::get_client_connection;
 use hyperqueue::transfer::messages::FromClientMessage;
 
+use crate::utils::error::ToPyResult;
 use crate::{borrow_mut, run_future, ContextPtr, HqContext};
 
 pub(crate) fn connect_to_server_impl(py: Python) -> PyResult<ContextPtr> {
@@ -17,7 +18,10 @@ pub(crate) fn connect_to_server_impl(py: Python) -> PyResult<ContextPtr> {
 pub(crate) fn stop_server_impl(py: Python, ctx: ContextPtr) -> PyResult<()> {
     run_future(async move {
         let mut ctx = borrow_mut!(py, ctx);
-        ctx.connection.send(FromClientMessage::Stop).await.unwrap();
+        ctx.connection
+            .send(FromClientMessage::Stop)
+            .await
+            .map_py_err()?;
         Ok(())
     })
 }
