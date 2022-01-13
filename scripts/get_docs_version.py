dd7dc4f57bbe42304518e312f38f2c5cff86adc8
Jakub Beránek <berykubik@gmail.com>
2022-01-13 11:04:54 +0100

    Check in CI that HyperQueue and its Python binding have the same version

--- scripts/get_docs_version.py
+++ scripts/get_docs_version.py
@@ -3,28 +3,28 @@ import subprocess
 
 
 def latest_version():
-    return {
-        "type": "latest"
-    }
+    return {"type": "latest"}
 
 
 def get_version(output: str):
     if not output:
         return latest_version()
     else:
-        tags = [t.strip() for t in output.splitlines(keepends=False) if t.startswith("v")]
+        tags = [
+            t.strip() for t in output.splitlines(keepends=False) if t.startswith("v")
+        ]
         if not tags:
             return latest_version()
-        return {
-            "type": "stable",
-            "version": sorted(tags, reverse=True)[0]
-        }
+        return {"type": "stable", "version": sorted(tags, reverse=True)[0]}
 
 
 if __name__ == "__main__":
     """
-    Calculates whether the current commit is a stable version (=there is some tag pointing to it) or an unstable one.
+    Calculates whether the current commit is a stable version (=there is some tag pointing to it) or
+    an unstable one.
     """
-    output = subprocess.check_output(["git", "tag", "--points-at", "HEAD"]).decode().strip()
+    output = (
+        subprocess.check_output(["git", "tag", "--points-at", "HEAD"]).decode().strip()
+    )
     version = get_version(output)
     print(json.dumps(version))
