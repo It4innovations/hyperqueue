67a2a474a624ef7deb74b9de2e0db26f8fe1deb6
Stanislav Bohm <spirali@kreatrix.org>
2022-02-01 16:20:36 +0100

    pyapi refactored

---
+++ crates/pyhq/python/hyperqueue/ffi/connection.py
@@ -0,0 +1,24 @@
+from typing import Optional
+
+from .. import hyperqueue as ffi
+from ..job import Job, JobId
+from .protocol import JobDescription
+
+
+class HqContext:
+    """
+    Opaque class returned from `connect_to_server`.
+    Should be passed to FFI methods that require it.
+    """
+
+
+class Connection:
+
+    def __init__(self, directory: Optional[str] = None):
+        self.ctx: HqContext = ffi.connect_to_server(directory)
+
+    def submit_job(self, job_description: JobDescription) -> JobId:
+        return ffi.submit_job(self.ctx, job_description)
+
+    def stop_server(self):
+        return ffi.stop_server(self.ctx)
--- crates/pyhq/python/hyperqueue/job.py
+++ crates/pyhq/python/hyperqueue/job.py
@@ -1,7 +1,7 @@
-from typing import List, Optional
+from typing import List, Optional, Sequence
 
 from .common import GenericPath
-from .task.config import TaskConfig, build_tasks
+from .ffi.protocol import JobDescription, TaskDescription
 from .task.program import EnvType, ExternalProgram, ProgramArgs
 from .task.task import Task
 
@@ -9,7 +9,7 @@ JobId = int
 
 
 class Job:
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         self.tasks: List[Task] = []
 
     def program(
@@ -17,12 +17,24 @@ class Job:
         args: ProgramArgs,
         env: EnvType = None,
         cwd: Optional[GenericPath] = None,
-        depends: List[Task] = None,
-        **kwargs
+        depends: Sequence[Task] = (),
     ) -> ExternalProgram:
         task = ExternalProgram(args=args, env=env, cwd=cwd, dependencies=depends)
         self.tasks.append(task)
         return task
 
-    def build(self) -> List[TaskConfig]:
-        return build_tasks(self.tasks)
+    def build(self) -> JobDescription:
+        id_map = {task: index for (index, task) in enumerate(self.tasks)}
+        task_descriptions = []
+        for task in self.tasks:
+            depends_on = [id_map[dependency] for dependency in task.dependencies]
+            task_descriptions.append(
+                TaskDescription(
+                    id=id_map[task],
+                    args=task.args,
+                    env=task.env,
+                    cwd=task.cwd,
+                    dependencies=depends_on,
+                )
+            )
+        return JobDescription(task_descriptions)
