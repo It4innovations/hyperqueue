2105b2089f7d266f24a0125d44ede9c0cacacaef
Alfred Koci <alfred.koci@vsb.cz>
2022-02-03 10:30:53 +0100

    first shot

---
+++ vizualization/events_type.py
@@ -0,0 +1,4 @@
+WorkerEvents = ['worker-connected', 'worker-lost']
+TasksEvents = ['task-started', "task-finished"]
+AlocattionEvents = ['allocation-qeue-created', 'allocation-qeue-finished', 'allocation-qeued',
+                    'allocation-qeue-removed']
---
+++ vizualization/hq_event_wrapper.py
@@ -0,0 +1,60 @@
+import subprocess as sp
+import os
+import json
+from events_type import *
+
+
+class Events(list):
+    def __init__(self, data):
+        self.extend(data)
+
+    def get_by_type(self, event_type) -> list:
+        if type(event_type) not in [list, tuple]:
+            event_type = [event_type]
+        return [i for i in self if i['type'] in event_type]
+
+    def get_by_worker_id(self, id) -> list:
+        return [i for i in self.get_by_type(WorkerEvents) if i['id'] == id]
+
+
+class HQEventWrapper:
+
+    def __init__(self, hq_event_bin_path: str, cwd: str = "", hq_bin_path: str = ""):
+        self.hq_bin_path = os.path.join(hq_bin_path, "hq")
+        self.hq_event_bin = hq_event_bin_path
+        self.cwd = cwd
+        self._get_events()
+
+    def _invoke_at(func):
+        def wrapper(self, *args, **kwargs):
+            cwd = os.getcwd()
+            os.chdir(self.cwd)
+            dec_func = func(self, *args, **kwargs)
+            os.chdir(cwd)
+            return dec_func
+
+        return wrapper
+
+    @_invoke_at
+    def _get_events(self) -> bytes:
+        output = sp.run([self.hq_bin_path, "event-log", "export", self.hq_event_bin], capture_output=True)
+        assert len(output.stdout) > 0
+        json_events = output.stdout
+        self.json_events_raw = json_events.decode("utf-8").split("\n")
+
+    def get_objects(self) -> Events:
+        objects = []
+        for row in self.json_events_raw:
+            if row != '':
+                event = json.loads(row)
+                if event['event']['type'] != 'worker-overview':
+                    event['event']['time'] = event['time']
+                    objects.append(event['event'])
+
+        return Events(objects)
+
+
+if __name__ == "__main__":
+    event_wrapper = HQEventWrapper("events.bin", "/home/fredy/IT4I/rust/hyperqueue", "target/debug")
+    eventy = event_wrapper.get_objects()
+    print(eventy)
