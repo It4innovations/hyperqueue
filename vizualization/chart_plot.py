651f9e07dc7c73af46a8afbd6b8e3809a3558b26
Alfred Koci <alfred.koci@vsb.cz>
2022-02-10 11:38:56 +0100

    basic alloc chart

--- vizualization/chart_plot.py
+++ vizualization/chart_plot.py
@@ -28,15 +28,39 @@ def events_to_workers_boxes(eventy, worker_ids):
     return pd.DataFrame(workers_boxes)
 
 
-def create_jobs_chart(eventy):
+def create_workers_chart(eventy):
     worker_ids = eventy.get_by_type(WorkerEvents).get_ids()
 
     workers_boxes = events_to_workers_boxes(eventy, worker_ids)
     workers_boxes['Worker'] = workers_boxes['Worker'].astype(str)
-
-    fig = px.timeline(workers_boxes, x_start="Start", x_end="Finish", y="Job_id", color="Worker",
-                      text='Job_str', facet_row='Worker')
+    fig = create_chart(workers_boxes, x_start="Start", x_end="Finish", y="Job_id", color="Worker", text='Job_str',
+                       facet_row='Worker')
+    # fig = px.timeline(workers_boxes, x_start="Start", x_end="Finish", y="Job_id", color="Worker",
+    #                   text='Job_str', facet_row='Worker')
     fig.update_yaxes(visible=False, matches=None)
+    fig.show()
+
+
+def create_chart(data, **kwargs):
+    return px.timeline(data, **kwargs)
+
+
+def create_alloc_chart(eventy):
+    ques = eventy.get_by_type('autoalloc-allocation-qued')
+    ques_id = ques.get_ids()
+    data = []
+    for id in ques_id:
+        qued = eventy.get_by_type('autoalloc-allocation-qued').get_by_id(id)[0]
+        que_started = eventy.get_by_type('autoalloc-allocation-started').get_by_id(id)[0]
+        que_finished = eventy.get_by_type('autoalloc-allocation-finished').get_by_id(id)[0]
+        que = [dict(id=f"qeue {id}", start=p.parse(qued.time), stop=p.parse(que_started.time), type='allocation-qeued'),
+               dict(id=f"qeue {id}", start=p.parse(que_started.time), stop=p.parse(que_finished.time),
+                    type='allocation-running')]
+        data.extend(que)
+
+    data = pd.DataFrame(data)
+    fig = create_chart(data, x_start="start", x_end="stop", y="id", color='type')
+    fig.u
     fig.show()
 
 
@@ -44,18 +68,36 @@ def create_jobs_chart(eventy):
 @click.option("--bin_file", default='events.bin')
 @click.option("--bin_pwd", default='../')
 @click.option('--hq_bin_file', default='target/debug')
-def create_timeline_chart_CLI(bin_file, bin_pwd, hq_bin_file):
+def create_workers_chart_CLI(bin_file, bin_pwd, hq_bin_file):
     event_wrapper = HQEventCLIWrapper(bin_file, bin_pwd, hq_bin_file)
     eventy = event_wrapper.get_objects()
-    create_jobs_chart(eventy)
+    create_workers_chart(eventy)
 
 
 @click.command(name="FILE-chart")
 @click.argument('json-file')
-def create_timeline_chart_JSON(json_file):
+def create_workers_chart_JSON(json_file):
     event_wrapper = HQEventFileWrapper(json_file)
     eventy = event_wrapper.get_objects()
-    create_jobs_chart(eventy)
+    create_workers_chart(eventy)
+
+
+@click.command(name="FILE-chart")
+@click.argument('json-file')
+def create_alloc_chart_JSON(json_file):
+    wrapper = HQEventFileWrapper(json_file)
+    eventy = wrapper.get_objects()
+    create_alloc_chart(eventy)
+
+
+@click.command(name="CLI-chart")
+@click.option("--bin_file", default='events.bin')
+@click.option("--bin_pwd", default='../')
+@click.option('--hq_bin_file', default='target/debug')
+def create_alloc_chart_CLI(bin_file, bin_pwd, hq_bin_file):
+    event_wrapper = HQEventCLIWrapper(bin_file, bin_pwd, hq_bin_file)
+    eventy = event_wrapper.get_objects()
+    create_alloc_chart(eventy)
 
 
 @click.group("jobs")
@@ -63,7 +105,6 @@ def cli_job():
     pass
 
 
-# TODO: create alloc events generator to simlute incoming data
 @click.group("alloc")
 def cli_alloc():
     pass
@@ -74,11 +115,15 @@ def cli():
     pass
 
 
-cli_job.add_command(create_timeline_chart_CLI)
-cli_job.add_command(create_timeline_chart_JSON)
+cli_job.add_command(create_workers_chart_CLI)
+cli_job.add_command(create_workers_chart_JSON)
 # cli_job()
 
+cli_alloc.add_command(create_alloc_chart_JSON)
+cli_alloc.add_command(create_alloc_chart_CLI)
 
 cli.add_command(cli_job)
 cli.add_command(cli_alloc)
 cli()
+
+
--- vizualization/events_type.py
+++ vizualization/events_type.py
@@ -1,4 +1,3 @@
 WorkerEvents = ['worker-connected', 'worker-lost']
 TasksEvents = ['task-started', "task-finished"]
-AlocattionEvents = ['allocation-qeue-created', 'allocation-qeue-finished', 'allocation-qeued',
-                    'allocation-qeue-removed']
+AlocattionEvents = ["autoalloc-allocation-qued", "autoalloc-allocation-started", "autoalloc-allocation-finished"]
