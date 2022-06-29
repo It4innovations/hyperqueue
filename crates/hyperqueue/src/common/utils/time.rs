a92ef9083f9f9f3eb6654802fdda2bd92154ddb9
Jakub Beránek <berykubik@gmail.com>
2022-06-29 11:38:36 +0200

    Port time parser to `chumsky`

--- crates/hyperqueue/src/common/utils/time.rs
+++ crates/hyperqueue/src/common/utils/time.rs
@@ -2,12 +2,9 @@ use std::time::{Duration, SystemTime};
 
 use anyhow::anyhow;
 use chrono::TimeZone;
-use nom::character::complete::char;
-use nom::combinator::{map_res, opt};
-use nom::sequence::{preceded, tuple};
-use nom_supreme::ParserExt;
-
-use crate::common::parser::{consume_all, p_u32, NomResult};
+use chumsky::error::Simple;
+use chumsky::prelude::just;
+use chumsky::Parser;
 
 // Allows specifying humantime format (2h, 3m, etc.)
 crate::arg_wrapper!(ArgDuration, Duration, humantime::parse_duration);
@@ -29,31 +26,29 @@ pub fn local_to_system_time(datetime: chrono::NaiveDateTime) -> SystemTime {
     chrono::Local.from_local_datetime(&datetime).unwrap().into()
 }
 
-fn p_hms_time(input: &str) -> NomResult<Duration> {
-    map_res(
-        tuple((
-            p_u32,
-            opt(preceded(char(':'), p_u32)),
-            opt(preceded(char(':'), p_u32)),
-        ))
-        .context("[[HH:]MM:]SS value"),
-        |parsed| match parsed {
-            (seconds, None, None) => Ok(Duration::from_secs(seconds as u64)),
-            (minutes, Some(seconds), None) => {
-                Ok(Duration::from_secs(minutes as u64 * 60 + seconds as u64))
-            }
-            (hours, Some(minutes), Some(seconds)) => Ok(Duration::from_secs(
-                hours as u64 * 3600 + minutes as u64 * 60 + seconds as u64,
-            )),
-            _ => Err(anyhow!("Invalid time specification")),
-        },
-    )(input)
+fn parse_hms_time_inner() -> impl CharParser<Duration> {
+    let digits = parse_u32();
+    let time2 = just(':').ignore_then(parse_u32()).or_not();
+    let time3 = just(':').ignore_then(parse_u32()).or_not();
+
+    let time = digits.then(time2).then(time3);
+    time.try_map(|parsed, span| match parsed {
+        ((seconds, None), None) => Ok(Duration::from_secs(seconds as u64)),
+        ((minutes, Some(seconds)), None) => {
+            Ok(Duration::from_secs(minutes as u64 * 60 + seconds as u64))
+        }
+        ((hours, Some(minutes)), Some(seconds)) => Ok(Duration::from_secs(
+            hours as u64 * 3600 + minutes as u64 * 60 + seconds as u64,
+        )),
+        _ => Err(Simple::custom(span, "Invalid time specification")),
+    })
+    .labelled("time in [[HH:]MM:]SS format")
 }
 
 /// Parses time strings in the format [[hh:]mm:]ss.
 /// Individual time values may be zero padded.
 pub fn parse_hms_time(input: &str) -> anyhow::Result<Duration> {
-    consume_all(p_hms_time, input)
+    all_consuming(parse_hms_time_inner()).parse_text(input)
 }
 
 #[cfg(not(test))]
@@ -61,6 +56,7 @@ pub fn now_monotonic() -> std::time::Instant {
     std::time::Instant::now()
 }
 
+use crate::common::parser2::{all_consuming, parse_u32, CharParser};
 #[cfg(test)]
 pub use mock_time::now_monotonic;
 
@@ -100,44 +96,64 @@ pub mod mock_time {
 
 #[cfg(test)]
 mod tests {
-    use crate::common::utils::time::{p_hms_time, parse_hms_time};
-    use crate::tests::utils::check_parse_error;
+    use crate::common::parser2::{all_consuming, CharParser};
+    use crate::common::utils::time::parse_hms_time_inner;
+    use crate::tests::utils::expect_parser_error;
 
     #[test]
     fn parse_hms_seconds() {
-        let duration = parse_hms_time("01").unwrap();
+        let duration = parse_hms_time_inner().parse_text("01").unwrap();
         assert_eq!(duration.as_secs(), 1);
 
-        let duration = parse_hms_time("1").unwrap();
+        let duration = parse_hms_time_inner().parse_text("1").unwrap();
         assert_eq!(duration.as_secs(), 1);
     }
 
     #[test]
     fn parse_hms_minutes() {
-        let duration = parse_hms_time("1:1").unwrap();
+        let duration = parse_hms_time_inner().parse_text("1:1").unwrap();
         assert_eq!(duration.as_secs(), 1 * 60 + 1);
 
-        let duration = parse_hms_time("80:02").unwrap();
+        let duration = parse_hms_time_inner().parse_text("80:02").unwrap();
         assert_eq!(duration.as_secs(), 80 * 60 + 2);
     }
 
     #[test]
     fn parse_hms_hours() {
-        let duration = parse_hms_time("1:1:1").unwrap();
+        let duration = parse_hms_time_inner().parse_text("1:1:1").unwrap();
         assert_eq!(duration.as_secs(), 1 * 3600 + 1 * 60 + 1);
 
-        let duration = parse_hms_time("02:03:04").unwrap();
+        let duration = parse_hms_time_inner().parse_text("02:03:04").unwrap();
         assert_eq!(duration.as_secs(), 2 * 3600 + 3 * 60 + 4);
     }
 
     #[test]
-    fn parse_hms_error() {
-        check_parse_error(
-            p_hms_time,
-            "x",
-            r#"Parse error
-expected [[HH:]MM:]SS value at character 0: "x"
-expected integer at character 0: "x""#,
-        );
+    fn parse_hms_no_number() {
+        insta::assert_snapshot!(expect_parser_error(parse_hms_time_inner(), "x"), @r###"
+        Unexpected token found while attempting to parse number, expected something else:
+          x
+          |
+          --- Unexpected token `x`
+        "###);
+    }
+
+    #[test]
+    fn parse_hms_trailing_colon() {
+        insta::assert_snapshot!(expect_parser_error(all_consuming(parse_hms_time_inner()), "12:"), @r###"
+        Unexpected end of input found while attempting to parse number, expected something else:
+          12:
+             |
+             --- Unexpected end of input
+        "###);
+    }
+
+    #[test]
+    fn parse_hms_minutes_no_number() {
+        insta::assert_snapshot!(expect_parser_error(all_consuming(parse_hms_time_inner()), "12:x"), @r###"
+        Unexpected token found while attempting to parse number, expected something else:
+          12:x
+             |
+             --- Unexpected token `x`
+        "###);
     }
 }
