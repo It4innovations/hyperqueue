4c926a6d582517972f03a2881386e0796370c50c
Jakub Beránek <berykubik@gmail.com>
2022-07-21 19:39:37 +0200

    Add basic Python documentation and API reference.
    Also sets up `nedoc` infrastructure for automatically rendering the documentation in `mkdocs` builds.

--- crates/pyhq/python/hyperqueue/cluster/__init__.py
+++ crates/pyhq/python/hyperqueue/cluster/__init__.py
@@ -9,29 +9,61 @@ from ..ffi.cluster import Cluster
 
 @dataclasses.dataclass
 class WorkerConfig:
+    """
+    Configuration of a worker spawned by a local cluster.
+    """
+
     # If None, use all available cores
     cores: Optional[int] = None
 
 
 class LocalCluster:
+    """
+    Represents a local deployed HyperQueue infrastructure.
+
+    You can use `LocalCluster` to quickly spin up a HyperQueue server along with a set of workers
+    locally.
+
+    The cluster can be used as a context manager. It will be stopped when the context ends:
+    ```python
+    with LocalCluster() as cluster:
+        client = cluster.client()
+        ...
+    # The cluster was stopped
+    ```
+    """
+
     def __init__(
         self,
         server_dir: Optional[Path] = None,
         worker_config: Optional[WorkerConfig] = None,
     ):
+        """
+        :param server_dir: Server directory where will the cluster store its files.
+        :param worker_config: Configuration of workers spawned in the cluster.
+        """
         self.cluster = Cluster(server_dir)
         if worker_config is not None:
             self.start_worker(worker_config)
 
     def start_worker(self, config: WorkerConfig = None):
+        """
+        Adds a new worker with the given `config` to the cluster.
+        """
         config = config if config is not None else WorkerConfig()
         cores = config.cores or multiprocessing.cpu_count()
         self.cluster.add_worker(cores)
 
     def client(self) -> Client:
+        """
+        Creates a client connected to this cluster.
+        """
         return Client(self.cluster.server_dir)
 
     def stop(self):
+        """
+        Stops the server and all workers of this cluster.
+        """
         self.cluster.stop()
 
     def __enter__(self):
--- crates/pyhq/python/hyperqueue/job.py
+++ crates/pyhq/python/hyperqueue/job.py
@@ -12,12 +12,22 @@ from .task.task import EnvType, Task
 
 
 class Job:
+    """
+    Represents a HQ job.
+    """
+
     def __init__(
         self,
         default_workdir: Optional[GenericPath] = None,
         max_fails: Optional[int] = 1,
         default_env: Optional[EnvType] = None,
     ):
+        """
+        :param default_workdir: Default working directory for tasks.
+        :param max_fails: How many tasks can fail before the whole job will be cancelled.
+        :param default_env: Environment variables that will be automatically set for each task in
+        this job.
+        """
         self.tasks: List[Task] = []
         self.task_map: Dict[TaskId, Task] = {}
         self.max_fails = max_fails
@@ -29,6 +39,9 @@ class Job:
         self.default_env = default_env or {}
 
     def task_by_id(self, id: TaskId) -> Optional[Task]:
+        """
+        Finds a task with the given ID.
+        """
         return self.task_map.get(id)
 
     def program(
@@ -46,6 +59,24 @@ class Job:
         priority: int = 0,
         resources: Optional[ResourceRequest] = None,
     ) -> ExternalProgram:
+        """
+        Creates a new task that will execute the provided command.
+
+        :param args: List of arguments will be executed. The arguments have to be strings.
+        :param env: Environment variables passed to the executed command.
+        :param cwd: Working directory of the executed command.
+        :param stdout: Path to a file that will store the standard output of the executed command.
+        :param stderr: Path to a file that will store the standard error output of the executed
+        command.
+        :param stdin: If provided, these bytes will be passed as the standard input of the executed
+        command.
+        :param deps: A sequence of dependencies that have to be completed first before this task
+        can start executing.
+        :param name: Name of the task.
+        :param task_dir: If True, an isolated directory will be created for the task.
+        :param priority: Priority of the created task.
+        :param resources: List of resource requests required by this task.
+        """
         task = ExternalProgram(
             len(self.tasks),
             args=args,
@@ -78,6 +109,22 @@ class Job:
         priority: int = 0,
         resources: Optional[ResourceRequest] = None,
     ) -> PythonFunction:
+        """
+        Creates a new task that will execute the provided Python function.
+
+        :param args: Positional arguments that will be passed to the Python function.
+        :param kwargs: Keyword arguments that will be passed to the Python function.
+        :param env: Environment variables passed to the executed command.
+        :param cwd: Working directory of the executed command.
+        :param stdout: Path to a file that will store the standard output of the executed command.
+        :param stderr: Path to a file that will store the standard error output of the executed
+        command.
+        :param deps: A sequence of dependencies that have to be completed first before this task
+        can start executing.
+        :param name: Name of the task.
+        :param priority: Priority of the created task.
+        :param resources: List of resource requests required by this task.
+        """
         task = PythonFunction(
             len(self.tasks),
             fn,
@@ -108,6 +155,10 @@ class Job:
 
 @dataclasses.dataclass
 class SubmittedJob:
+    """
+    Successfully submitted job.
+    """
+
     job: Job
     id: JobId
 
