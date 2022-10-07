33a32c217c2baed8692013962c026529b8f1339d
Jakub Beránek <berykubik@gmail.com>
2022-10-07 11:03:25 +0200

    Introduce new system for mocking autoalloc tests

--- tests/utils/io.py
+++ tests/utils/io.py
@@ -1,3 +1,7 @@
+import socket
+from contextlib import closing
+
+
 def check_file_contents(path: str, content):
     assert read_file(path) == str(content)
 
@@ -5,3 +9,10 @@ def check_file_contents(path: str, content):
 def read_file(path: str) -> str:
     with open(path) as f:
         return f.read()
+
+
+def find_free_port():
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+        s.bind(("", 0))
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        return s.getsockname()[1]
--- tests/utils/mock.py
+++ tests/utils/mock.py
@@ -1,26 +1,33 @@
 import contextlib
 import os
 import sys
-from os.path import isfile, join
+from pathlib import Path
 from typing import Dict
 
 
 class ProgramMock:
     def __init__(self, directory: str):
-        self.directory = str(os.path.abspath(directory))
+        self.directory = Path(os.path.abspath(directory))
         os.makedirs(self.directory, exist_ok=True)
 
     def update_env(self, env: Dict[str, str]):
-        path = self.directory
+        path = str(self.directory)
         if "PATH" in env:
             path += f":{env['PATH']}"
         env["PATH"] = path
 
+    def mock(self, name: str, target: Path):
+        link_path = self.directory / name
+        link_path.unlink(missing_ok=True)
+        os.symlink(dst=link_path, src=target)
+        # Make the link executable
+        os.chmod(link_path, 0o700)
+
     @contextlib.contextmanager
     def mock_program(self, name: str, code: str):
         content = f"#!{sys.executable}\n{code}"
-        program_path = join(self.directory, name)
-        assert not isfile(program_path)
+        program_path = self.directory / name
+        assert not program_path.is_file()
 
         with open(program_path, "w") as f:
             f.write(content)
