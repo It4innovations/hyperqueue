36d8776c80a95d11102a68ea20e542ebeee3aaaf
Ada Bohm <ada@kreatrix.org>
2023-12-08 16:13:51 +0100

    Allow combining --array and --each-line/--from-json

--- tests/test_entries.py
+++ tests/test_entries.py
@@ -110,3 +110,65 @@ def test_entries_invalid_from_json_entry(hq_env: HqEnv):
                 "echo $HQ_ENTRY",
             ]
         )
+
+
+def test_each_line_with_array(hq_env: HqEnv):
+    hq_env.start_server()
+    hq_env.start_worker(cpus=2)
+
+    with open("input", "w") as f:
+        f.write("One\nTwo\nThree\nFour\nFive\nSix\nSeven")
+
+    hq_env.command(
+        [
+            "submit",
+            "--each-line=input",
+            "--array", "2-4, 6",
+            "--",
+            "bash",
+            "-c",
+            "echo $HQ_ENTRY,$HQ_TASK_ID",
+        ]
+    )
+    wait_for_job_state(hq_env, 1, "FINISHED")
+
+    for i, test in enumerate([None, None, "Three,2\n", "Four,3\n", "Five,4\n", None, "Seven,6\n"]):
+        filename = default_task_output(job_id=1, task_id=i)
+        if test is None:
+            assert not os.path.exists(filename)
+        else:
+            check_file_contents(filename, test)
+
+    table = hq_env.command(["job", "info", "1"], as_table=True)
+    assert table.get_row_value("State").split("\n")[-1] == "FINISHED (4)"
+
+
+def test_json_with_array(hq_env: HqEnv):
+    hq_env.start_server()
+    hq_env.start_worker(cpus=2)
+
+    with open("input", "w") as f:
+        f.write('["One", "Two", "Three", "Four", "Five", "Six", "Seven"]')
+
+    hq_env.command(
+        [
+            "submit",
+            "--from-json=input",
+            "--array", "2-3, 5, 6, 7, 1000",
+            "--",
+            "bash",
+            "-c",
+            "echo $HQ_ENTRY,$HQ_TASK_ID",
+        ]
+    )
+    wait_for_job_state(hq_env, 1, "FINISHED")
+
+    for i, test in enumerate([None, None, '"Three",2\n', '"Four",3\n', None, '"Six",5\n', '"Seven",6\n']):
+        filename = default_task_output(job_id=1, task_id=i)
+        if test is None:
+            assert not os.path.exists(filename)
+        else:
+            check_file_contents(filename, test)
+
+    table = hq_env.command(["job", "info", "1"], as_table=True)
+    assert table.get_row_value("State").split("\n")[-1] == "FINISHED (4)"
