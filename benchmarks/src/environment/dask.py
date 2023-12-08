beb2f7d4ad5ea04ac27b96e4a20cefd1f0fd7ebb
Jakub Beránek <berykubik@gmail.com>
2023-12-08 13:01:57 +0100

    Start Dask processes manually instead of using `LocalCluster`
    This will make it easier to monitor the Dask processes, and it also allows us to redirect stdout/stderr to files and spawn Dask workers on multiple nodes.

--- benchmarks/src/environment/dask.py
+++ benchmarks/src/environment/dask.py
@@ -1,16 +1,20 @@
-import dataclasses
 import logging
+import socket
+import subprocess
+from contextlib import closing
 from pathlib import Path
 from typing import Any, Dict, Optional, List
+
+import dataclasses
+from cluster.cluster import HOSTNAME
+from dask.distributed import Client
 
 from . import Environment, EnvironmentDescriptor
 from .utils import EnvStateManager
 from ..clusterutils import ClusterInfo
-
-from dask.distributed import Client, LocalCluster
-
-from ..clusterutils.cluster_helper import ClusterHelper
+from ..clusterutils.cluster_helper import ClusterHelper, StartProcessArgs
 from ..clusterutils.node_list import Local
+from ..utils import ensure_directory
 
 
 @dataclasses.dataclass(frozen=True)
@@ -45,13 +49,19 @@ class DaskClusterInfo(EnvironmentDescriptor):
         return {}
 
 
+def find_free_port() -> int:
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+        s.bind(("", 0))
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        return s.getsockname()[1]
+
+
 class DaskEnvironment(Environment, EnvStateManager):
     def __init__(self, info: DaskClusterInfo, workdir: Path):
         super().__init__()
         self.info = info
         self._workdir = workdir.resolve()
         self.cluster = ClusterHelper(info.cluster_info, workdir=self.workdir)
-        self.local_cluster: Optional[LocalCluster] = None
         self.client: Optional[Client] = None
 
     def __enter__(self):
@@ -73,27 +83,54 @@ class DaskEnvironment(Environment, EnvStateManager):
 
         worker_count = len(self.info.workers)
         assert worker_count == 1
-        self.local_cluster = LocalCluster(
-            n_workers=self.info.workers[0].processes,
-            threads_per_worker=self.info.workers[0].threads_per_process,
-            resources=dict(cores=self.info.workers[0].threads_per_process),
-            dashboard_address=None,
+
+        server_port = find_free_port()
+        dask_dir = ensure_directory(self.workdir / "dask")
+
+        # Make sure to kill all previously running Dask instances
+        subprocess.run(["killall", "dask"], check=False)
+
+        server_address = f"{HOSTNAME}:{server_port}"
+        scheduler = StartProcessArgs(
+            args=["dask", "scheduler", "--no-dashboard", "--host", HOSTNAME, "--port", str(server_port)],
+            workdir=ensure_directory(dask_dir / "server"),
+            hostname=HOSTNAME,
+            name="server",
         )
-        self.client = self.local_cluster.get_client()
+        self.cluster.start_processes([scheduler])
+
+        workers = [
+            StartProcessArgs(
+                args=[
+                    "dask",
+                    "worker",
+                    "--nworkers",
+                    str(worker.processes),
+                    "--nthreads",
+                    str(worker.threads_per_process),
+                    "--no-dashboard",
+                    "--resources",
+                    f"cores={worker.threads_per_process}",
+                    server_address,
+                ],
+                workdir=ensure_directory(dask_dir / f"worker-{index}"),
+                hostname=HOSTNAME,
+                name="worker",
+            )
+            for (index, worker) in enumerate(self.info.workers)
+        ]
+        self.cluster.start_processes(workers)
+
+        self.client = Client(server_address)
         self.client.wait_for_workers(n_workers=worker_count)
 
-        self.cluster.start_monitoring(self.info.cluster_info.node_list.resolve())
+        self.cluster.start_monitoring(self.info.cluster_info.node_list.resolve(), observe_processes=True)
         self.cluster.commit()
         logging.info("Dask cluster started")
 
     def stop(self):
         self.state_stop()
-
-        self.client.close()
-
-        assert self.local_cluster
-        self.local_cluster.close()
-
+        self.client.shutdown()
         self.cluster.stop(use_sigint=True)
 
     def get_client(self) -> Client:
