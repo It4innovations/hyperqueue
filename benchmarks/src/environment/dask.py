fb99521d1f40f4cc0afd2ca404fa7625c97434fe
Jakub Beránek <jakub.beranek@vsb.cz>
2024-07-18 11:38:42 +0200

    Generalize Dask environment for multiple nodes

--- benchmarks/src/environment/dask.py
+++ benchmarks/src/environment/dask.py
@@ -1,8 +1,11 @@
+import datetime
 import logging
+import os
 import socket
 import subprocess
 from contextlib import closing
 from pathlib import Path
+import time
 from typing import Any, Dict, Optional, List
 
 import dataclasses
@@ -10,11 +13,14 @@ from cluster.cluster import HOSTNAME
 from dask.distributed import Client
 
 from . import Environment, EnvironmentDescriptor
-from .utils import EnvStateManager
+from .utils import EnvStateManager, assign_workers, sanity_check_nodes
 from ..clusterutils import ClusterInfo
 from ..clusterutils.cluster_helper import ClusterHelper, StartProcessArgs
 from ..clusterutils.node_list import Local
 from ..utils import ensure_directory
+
+# <hq-root>/benchmarks
+BENCHMARKS_DIR = Path(__file__).absolute().parent.parent.parent
 
 
 @dataclasses.dataclass(frozen=True)
@@ -25,6 +31,15 @@ class DaskWorkerConfig:
 
     processes: int
     threads_per_process: int
+    node: Optional[int] = None
+    init_cmd: List[str] = dataclasses.field(default_factory=list)
+
+    @staticmethod
+    def create(count: int, n_processes: int = 1, **kwargs) -> "DaskWorkerConfig":
+        assert count % n_processes == 0
+        return DaskWorkerConfig(
+            processes=n_processes, threads_per_process=count // n_processes, **kwargs
+        )
 
 
 @dataclasses.dataclass(frozen=True)
@@ -63,13 +78,23 @@ class DaskEnvironment(Environment, EnvStateManager):
         self._workdir = workdir.resolve()
         self.cluster = ClusterHelper(info.cluster_info, workdir=self.workdir)
         self.client: Optional[Client] = None
+        self.dask_dir = ensure_directory(self.workdir / "dask")
 
-    def __enter__(self):
-        self.start()
-        return self
+        self.nodes = self.info.cluster_info.node_list.resolve()
+        sanity_check_nodes(self.nodes)
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.stop()
+        worker_nodes = (
+            self.nodes
+            if isinstance(self.info.cluster_info.node_list, Local)
+            else self.nodes[1:]
+        )
+        if not worker_nodes:
+            raise Exception("No worker nodes are available")
+
+        self.worker_count = sum(w.processes for w in self.info.workers)
+        assert self.worker_count > 0
+        self.worker_assignment = assign_workers(self.info.workers, worker_nodes)
+        logging.debug(f"Worker assignment: {self.worker_assignment}")
 
     @property
     def workdir(self) -> Path:
@@ -79,59 +104,101 @@ class DaskEnvironment(Environment, EnvStateManager):
         self.state_start()
         logging.info("Starting Dask cluster")
 
-        assert isinstance(self.info.cluster_info.node_list, Local)
-
-        worker_count = len(self.info.workers)
-        assert worker_count == 1
-
         server_port = find_free_port()
-        dask_dir = ensure_directory(self.workdir / "dask")
 
         # Make sure to kill all previously running Dask instances
         subprocess.run(["killall", "dask"], check=False)
 
-        server_address = f"{HOSTNAME}:{server_port}"
-        scheduler = StartProcessArgs(
-            args=["dask", "scheduler", "--no-dashboard", "--host", HOSTNAME, "--port", str(server_port)],
-            workdir=ensure_directory(dask_dir / "server"),
-            hostname=HOSTNAME,
-            name="server",
-        )
-        self.cluster.start_processes([scheduler])
+        self._start_scheduler(server_port)
 
-        workers = [
-            StartProcessArgs(
-                args=[
-                    "dask",
-                    "worker",
-                    "--nworkers",
-                    str(worker.processes),
-                    "--nthreads",
-                    str(worker.threads_per_process),
-                    "--no-dashboard",
-                    "--resources",
-                    f"cores={worker.threads_per_process}",
-                    server_address,
-                ],
-                workdir=ensure_directory(dask_dir / f"worker-{index}"),
-                hostname=HOSTNAME,
-                name="worker",
-            )
-            for (index, worker) in enumerate(self.info.workers)
-        ]
-        self.cluster.start_processes(workers)
+        server_address = f"{HOSTNAME}:{server_port}"
+        self._start_workers(self.worker_assignment, server_address)
 
         self.client = Client(server_address)
-        self.client.wait_for_workers(n_workers=worker_count)
+        self.client.wait_for_workers(n_workers=self.worker_count)
+        time.sleep(1)
+        assert len(self.client.scheduler_info()["workers"]) == self.worker_count
 
-        self.cluster.start_monitoring(self.info.cluster_info.node_list.resolve(), observe_processes=True)
+        self.cluster.start_monitoring(
+            self.info.cluster_info.node_list.resolve(), observe_processes=True
+        )
         self.cluster.commit()
         logging.info("Dask cluster started")
 
+    def _start_scheduler(self, server_port: int):
+        scheduler = StartProcessArgs(
+            args=[
+                "dask",
+                "scheduler",
+                "--no-dashboard",
+                "--host",
+                HOSTNAME,
+                "--port",
+                str(server_port),
+            ],
+            workdir=ensure_directory(self.dask_dir / "server"),
+            hostname=HOSTNAME,
+            name="server",
+            env=self._get_shared_envs(),
+        )
+        self.cluster.start_processes([scheduler])
+
+    def _start_workers(
+        self, worker_assignment: Dict[str, List[DaskWorkerConfig]], server_address: str
+    ):
+        worker_processes = []
+
+        items = sorted(worker_assignment.items(), key=lambda item: item[0])
+        for node, workers in items:
+            for worker_index, worker in enumerate(workers):
+                worker: DaskWorkerConfig = worker
+
+                worker_index = f"worker-{node}-{worker_index}"
+                workdir = self.dask_dir / worker_index
+                args = StartProcessArgs(
+                    args=[
+                        "dask",
+                        "worker",
+                        "--nworkers",
+                        str(worker.processes),
+                        "--nthreads",
+                        str(worker.threads_per_process),
+                        "--no-dashboard",
+                        "--local-directory",
+                        "/tmp",
+                        server_address,
+                    ],
+                    workdir=workdir,
+                    hostname=node,
+                    name="worker",
+                    env=self._get_shared_envs(),
+                    init_cmd=worker.init_cmd,
+                )
+                worker_processes.append(args)
+
+        self.cluster.start_processes(worker_processes)
+
     def stop(self):
         self.state_stop()
+
+        logging.info("Waiting for Dask cluster to shutdown")
         self.client.shutdown()
+        self.cluster.wait_for_process_end(
+            lambda p: p.key == "server" or p.key.startswith("worker"),
+            duration=datetime.timedelta(seconds=5),
+        )
         self.cluster.stop(use_sigint=True)
 
     def get_client(self) -> Client:
         return self.client
+
+    def _get_shared_envs(self) -> Dict[str, str]:
+        pythonpath = os.environ.get("PYTHONPATH", "")
+        if len(pythonpath) > 0:
+            pythonpath += ":"
+        env = dict(PYTHONPATH=f"{pythonpath}{BENCHMARKS_DIR}")
+
+        virtualenv = os.environ.get("VIRTUAL_ENV")
+        if virtualenv is not None:
+            env["VIRTUAL_ENV"] = os.path.abspath(virtualenv)
+        return env
--- benchmarks/src/environment/utils.py
+++ benchmarks/src/environment/utils.py
@@ -1,3 +1,7 @@
+from collections import defaultdict
+from typing import Dict, List, Optional, Protocol
+
+
 class Init:
     pass
 
@@ -26,3 +30,45 @@ class EnvStateManager:
     def state_stop(self):
         assert isinstance(self.state, Started)
         self.state = Stopped()
+
+
+def sanity_check_nodes(nodes: List[str]):
+    for node in nodes:
+        assert len(node) > 0
+    assert len(set(nodes)) == len(nodes)
+    assert len(nodes) > 0
+
+
+class WorkerConfig(Protocol):
+    node: Optional[int]
+
+
+def assign_workers(
+    workers: List[WorkerConfig], nodes: List[str]
+) -> Dict[str, List[WorkerConfig]]:
+    round_robin_node = 0
+    used_round_robin = set()
+
+    node_assignments = defaultdict(list)
+    for index, worker in enumerate(workers):
+        node = worker.node
+        if node is not None:
+            if not (0 <= node < len(nodes)):
+                raise Exception(
+                    f"Invalid node assignment. Worker {index} wants to be on node "
+                    f"{node}, but there are only {len(nodes)} worker nodes"
+                )
+        else:
+            node = round_robin_node
+            round_robin_node = (round_robin_node + 1) % len(nodes)
+            if node in used_round_robin:
+                raise Exception(
+                    f"There are more workers ({len(workers)}) than worker nodes ({len(nodes)})"
+                )
+            used_round_robin.add(node)
+        if node >= len(nodes):
+            raise Exception(
+                f"Selected worker node is {node}, but there are only {len(nodes)} worker node(s)"
+            )
+        node_assignments[nodes[node]].append(worker)
+    return dict(node_assignments)
