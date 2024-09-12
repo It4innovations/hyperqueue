397cacd7a32c56cbfbd89dca7c23dedbbb76755b
Ada Bohm <ada@kreatrix.org>
2024-09-12 07:43:43 +0200

    Fixed error message

--- crates/hyperqueue/src/client/commands/submit/command.rs
+++ crates/hyperqueue/src/client/commands/submit/command.rs
@@ -804,7 +804,7 @@ fn warn_array_task_count(opts: &JobSubmitOpts, task_count: u32) {
     if task_files > SUBMIT_ARRAY_LIMIT && opts.conf.stream.is_none() {
         log::warn!(
             "The job will create {} files for {}. \
-            Consider using the `--log` option to stream all outputs into a single file",
+            Consider using the `--stream` option to stream all outputs into a single file",
             task_files,
             active_dirs.join(" and ")
         );
