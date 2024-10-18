a2bece1bc5255dea322d900e0e1c7c231ebbc81d
Jakub Beránek <jakub.beranek@vsb.cz>
2024-10-18 14:00:46 +0200

    Improve layout of root screen and make worker screen the default

--- crates/hyperqueue/src/dashboard/mod.rs
+++ crates/hyperqueue/src/dashboard/mod.rs
@@ -8,4 +8,8 @@ pub use ui_loop::start_ui_loop;
 
 use std::time::Duration;
 
+// The time range in which the live timeline is display ([now() - duration, now()])
 const DEFAULT_LIVE_DURATION: Duration = Duration::from_secs(60 * 10);
+
+// The amount of time that is subtracted/added when moving the timeline
+const TIMELINE_MOVE_OFFSET: Duration = Duration::from_secs(60 * 5);
