9f64827d7e71fe848d73195317d110c8833c76b9
Jakub Beránek <berykubik@gmail.com>
2025-02-28 12:56:10 +0100

    Get rid of `WrappedDuration`
    `pyo3` now supports `Duration` serialization out of the box

--- crates/pyhq/src/marshal.rs
+++ crates/pyhq/src/marshal.rs
@@ -42,28 +42,3 @@ where
             .map_err(|e| e.into())
     }
 }
-
-#[derive(Debug)]
-pub struct WrappedDuration(Duration);
-
-impl<'a> FromPyObject<'a> for WrappedDuration {
-    fn extract(object: &'a PyAny) -> PyResult<Self> {
-        if !object.is_instance_of::<PyFloat>() {
-            return Err(anyhow::anyhow!(
-                "Duration must be represented as a float containing the total amount of seconds."
-            ))
-            .map_err(|e| e.into());
-        }
-
-        let seconds: f64 = object.extract()?;
-        let nanoseconds = seconds * 1e9;
-
-        Ok(WrappedDuration(Duration::from_nanos(nanoseconds as u64)))
-    }
-}
-
-impl From<WrappedDuration> for Duration {
-    fn from(value: WrappedDuration) -> Self {
-        value.0
-    }
-}
