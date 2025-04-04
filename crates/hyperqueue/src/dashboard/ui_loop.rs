1c7ed4ccfaf402539584ea2cb5c0907e1dde2894
Jakub Beránek <berykubik@gmail.com>
2025-04-04 14:30:11 +0200

    Sort preloaded events in dashboard

--- crates/hyperqueue/src/dashboard/ui_loop.rs
+++ crates/hyperqueue/src/dashboard/ui_loop.rs
@@ -31,10 +31,14 @@ pub async fn start_ui_loop(events: PreloadedEvents) -> anyhow::Result<()> {
         PreloadedEvents::FromServer { .. } => true,
     };
     let mut dashboard_data = DashboardData::new(time_mode, stream);
-    let (events, session) = match events {
+    let (mut events, session) = match events {
         PreloadedEvents::FromJournal(events) => (events, None),
         PreloadedEvents::FromServer { events, connection } => (events, Some(connection)),
     };
+
+    // Make sure that the events are sorted, to avoid pathological performance in the time-sorted
+    // Vecs.
+    events.sort_unstable_by_key(|e| e.time);
     dashboard_data.push_new_events(events);
 
     let mut root_screen = RootScreen::default();
