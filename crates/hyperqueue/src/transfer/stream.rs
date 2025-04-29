b6e1ba7fb3210a9476e64719faf123e6bb09cd8e
Ada Bohm <ada@kreatrix.org>
2025-04-29 10:34:54 +0200

    Removed forgotten structs

--- crates/hyperqueue/src/transfer/stream.rs
+++ crates/hyperqueue/src/transfer/stream.rs
@@ -2,33 +2,9 @@ use chrono::serde::ts_milliseconds;
 use chrono::{DateTime, Utc};
 use serde::Deserialize;
 use serde::Serialize;
-use tako::{InstanceId, JobId, JobTaskId, TaskId};
+use tako::{InstanceId, TaskId};
 
 pub type ChannelId = u32;
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct StreamTaskStart {
-    pub job: JobId,
-    pub task: JobTaskId,
-    pub instance: InstanceId,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct StreamData {
-    pub job: JobId,
-    pub task: JobTaskId,
-    pub instance: InstanceId,
-    pub channel: ChannelId,
-    #[serde(with = "serde_bytes")]
-    pub data: Vec<u8>,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct StreamTaskEnd {
-    pub job: JobId,
-    pub task: JobTaskId,
-    pub instance: InstanceId,
-}
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct StreamChunkHeader {
