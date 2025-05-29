3b053372c93df12aca4f2da0f21161c40c4a6c59
Ada Bohm <ada@kreatrix.org>
2025-05-29 16:14:48 +0200

    New hq journal versioning

--- crates/hyperqueue/src/server/event/journal/mod.rs
+++ crates/hyperqueue/src/server/event/journal/mod.rs
@@ -4,7 +4,17 @@ mod stream;
 mod write;
 
 pub use read::JournalReader;
+use serde::{Deserialize, Serialize};
 pub use stream::{EventStreamMessage, EventStreamSender, start_event_streaming};
 pub use write::JournalWriter;
 
-const HQ_JOURNAL_HEADER: &[u8] = b"hqjl0001";
+const HQ_JOURNAL_HEADER: &[u8] = b"hqjl0002";
+
+const HQ_JOURNAL_VERSION_MAJOR: u32 = 23;
+const HQ_JOURNAL_VERSION_MINOR: u32 = 0;
+
+#[derive(Serialize, Deserialize)]
+struct JournalVersion {
+    major: u32,
+    minor: u32,
+}
