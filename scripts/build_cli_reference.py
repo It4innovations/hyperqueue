89284ddcf59a04860ec5a3d13b3020b2c5e5a3c6
Jakub Beránek <berykubik@gmail.com>
2025-06-27 14:58:49 +0200

    Rewrite `build_cli_reference.py` using mkdocs hooks

--- scripts/build_cli_reference.py
+++ scripts/build_cli_reference.py
@@ -7,14 +7,15 @@ import shutil
 import subprocess
 import tempfile
 
-import mkdocs_gen_files
+from mkdocs.config import Config
+from mkdocs.structure.files import File, Files
 
 
 def is_cli_doc_available() -> bool:
     return shutil.which("cli_doc") is not None
 
 
-def run():
+def on_files(files: Files, config: Config):
     if not is_cli_doc_available():
         if os.environ.get("GITHUB_ACTIONS", "0") == "1":
             raise Exception("cli_doc not available in CI")
@@ -26,9 +27,6 @@ def run():
 
     with tempfile.NamedTemporaryFile() as tmp_file:
         subprocess.run(["cli_doc", hq_path, "--output-filename", tmp_file.name], check=True)
-        with mkdocs_gen_files.open("cli-reference/index.html", "w") as dst:
-            with open(tmp_file.name) as src:
-                dst.write(src.read())
-
-
-run()
+        with open(tmp_file.name) as src:
+            content = src.read()
+        files.append(File.generated(config, "cli-reference/index.html", content=content))
