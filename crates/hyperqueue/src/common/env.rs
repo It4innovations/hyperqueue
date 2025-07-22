e1a0bd85c5c87ba7a2bda67854d3f30563eebbc8
Ada Bohm <ada@kreatrix.org>
2025-07-22 12:14:58 +0200

    Refactor access tokens to be usable outside of data transfers

--- crates/hyperqueue/src/common/env.rs
+++ crates/hyperqueue/src/common/env.rs
@@ -25,4 +25,4 @@ pub const HQ_CPUS: &str = create_hq_env!("CPUS");
 pub const HQ_NODE_FILE: &str = create_hq_env!("NODE_FILE");
 pub const HQ_HOST_FILE: &str = create_hq_env!("HOST_FILE");
 pub const HQ_NUM_NODES: &str = create_hq_env!("NUM_NODES");
-pub const HQ_DATA_ACCESS: &str = create_hq_env!("DATA_ACCESS");
+pub const HQ_TOKEN: &str = create_hq_env!("TOKEN");
