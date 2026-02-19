9ea431d3bde7f9b10a28c9d98d4c0f513a34fb82
Ada Bohm <ada@kreatrix.org>
2026-02-19 10:00:15 +0100

    Old scheduler removed; scheduler2 renamed to scheduler

--- crates/tako/src/internal/tests/mod.rs
+++ crates/tako/src/internal/tests/mod.rs
@@ -3,8 +3,6 @@ mod test_query;
 #[cfg(test)]
 mod test_reactor;
 #[cfg(test)]
-mod test_scheduler2_sn;
-#[cfg(test)]
 mod test_scheduler_mapping;
 #[cfg(test)]
 mod test_scheduler_sn;
@@ -12,5 +10,5 @@ mod test_scheduler_sn;
 mod test_worker;
 
 pub mod integration;
-mod test_scheduler2_mn;
+mod test_scheduler_mn;
 pub mod utils;
