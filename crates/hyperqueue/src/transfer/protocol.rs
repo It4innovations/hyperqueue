abbaae1b4a5631c010cfe64cd16631d9c7e61b07
Ada Bohm <ada@kreatrix.org>
2026-03-23 11:32:26 +0100

    Removes commented code

--- crates/hyperqueue/src/transfer/protocol.rs
+++
@@ -1,8 +0,0 @@
-// use tokio_util::codec::LengthDelimitedCodec;
-// use tokio_util::codec::length_delimited::Builder;
-//
-// pub fn make_protocol_builder() -> Builder {
-//     *LengthDelimitedCodec::builder()
-//         .little_endian()
-//         .max_frame_length(tako::MAX_FRAME_SIZE)
-// }
